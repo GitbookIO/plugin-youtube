0931ae60d258697db5c477fadb9e6d699f057659
Johan Preynat <johan.preynat@gmail.com>
2016-10-10 18:19:34 +0200

    Update server-side block to return the correct props for component

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-var getYouTubeID = require('get-youtube-id');
+const getYouTubeID = require('get-youtube-id');
 
 module.exports = {
     blocks: {
-        youtube: {
-            process: function(blk) {
-                var videoId = getYouTubeID(blk.body) || blk.body;
-                var url = "http://youtube.com/watch?v="+videoId;
-                var videoThumb = 'http://img.youtube.com/vi/'+videoId+'/0.jpg' /* 0.jpg is default full resolution image. 1–3.jpg is thumbnails */
+        youtube: (block) => {
+            console.log('in block youtube');
+            console.log(block);
+            const videoId = getYouTubeID(block.kwargs.src) || block.kwargs.src;
+            const url     = `http://youtube.com/watch?v=${videoId}`;
 
-                if (this.generator != "website") {
-                    return '<img src="'+videoThumb+'" alt="YouTube video thumbnail" /><p><a href="'+url+'">Video link</a></p>';
-                }
+            const videoThumb = `http://img.youtube.com/vi/${videoId}/0.jpg`; /* 0.jpg is default full resolution image. 1–3.jpg is thumbnails */
 
-                return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">'
-                +'<iframe frameborder="0" allowfullscreen style="border: none;position: absolute;top: 0;left: 0;width: 100%;height: 100%;" src="//www.youtube.com/embed/'+videoId+'"></iframe>'
-                +'</div>';
-            }
+            return {
+                youtubeUrl: this.output.name != 'website' ? videoThumb : url
+            };
         }
     }
 };
