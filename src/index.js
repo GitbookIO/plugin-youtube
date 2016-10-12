eed3cdbeb1e62b6dbfc4a1c025b0bcc9e8429a4f
Johan Preynat <johan.preynat@gmail.com>
2016-10-12 15:51:59 +0200

    Update component to render thumbnail for ebook versions

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ const { React } = GitBook;
 
 const YoutubeBlock = React.createClass({
     propTypes: {
-        youtubeUrl: React.PropTypes.string.isRequired
+        isVideo:   React.PropTypes.bool.isRequired,
+        url:       React.PropTypes.string.isRequired,
+        thumbnail: React.PropTypes.string.isRequired
     },
 
     render() {
-        const { youtubeUrl } = this.props;
+        const { isVideo, url, thumbnail } = this.props;
 
         return (
-            <div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">
-                <iframe src={`//www.youtube.com/embed/${youtubeUrl}`}
+            <div className="YoutubeBlock">
+                <GitBook.ImportCSS href="gitbook/youtube/youtube.css" />
+            { isVideo ?
+                <iframe src={url}
                     frameBorder="0"
-                    allowFullScreen
-                    style="border: none;position: absolute;top: 0;left: 0;width: 100%;height: 100%;"></iframe>
+                    allowFullScreen></iframe>
+                :
+                <img src={thumbnail} />
+            }
             </div>
         );
     }
